a7ead5d1c96fd7e1a35a9d72c7550cbdb0386c25
EduardoBrant <carlos23brant@hotmail.com>
2023-12-15 19:29:05 -0400

    new commit

--- TeenyChain.java
+++ TeenyChain.java
@@ -1,9 +1,6 @@
 import java.util.ArrayList;
 import java.security.Security;
-import java.util.Base64;
 import java.util.HashMap;
-
-import com.google.gson.GsonBuilder;
 
 public class TeenyChain {
 

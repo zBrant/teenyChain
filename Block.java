ebabd47bb52197c2c7280b486a2bf03eae996530
EduardoBrant <carlos23brant@hotmail.com>
2023-12-15 19:26:02 -0400

    new commit

--- Block.java
+++ Block.java
@@ -1,17 +1,16 @@
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Block {
 
   public String hash;
   public String previousHash;
+  public String merkleRoot;
+  public ArrayList<Transaction> transactions = new ArrayList<>();
   private long timeStamp;
   private int nonce;
-
-  // A simple data 
-  private String data;
   
-  public Block(String data, String previousHash){
-    this.data = data;
+  public Block(String previousHash){
     this.previousHash = previousHash;
     this.timeStamp = new Date().getTime();
     this.hash = calculateHash();
@@ -22,18 +21,32 @@ public class Block {
       previousHash +
       Long.toString(timeStamp) +
       Integer.toString(nonce) +
-      data
+      merkleRoot
     );
     return calculateHash;
   } 
 
   public void mineBlock(int difficulty){
+    merkleRoot = StringUtil.getMerkleRoot(transactions);
     // String with difficulty * '0'
-    String target = new String(new char[difficulty]).replace('\0','0');
+    String target = new String(new char[difficulty]).replace('\0', '0');
     while (!hash.substring(0, difficulty).equals(target)) {
       nonce++;
       hash = calculateHash();
     }
     System.out.println("Block Mined!! : " + hash );
+  }
+
+  public boolean addTransaction(Transaction transaction){
+    if(transaction == null) return false;
+    if(previousHash != "0" && transaction.processTransaction() != true){
+      System.out.println("Transaction failed to process. Discated");
+      return false;
+    }
+    
+    transactions.add(transaction);
+    System.out.println("Transaction Successfilly added to Block");
+    return true;
+
   }
 }
